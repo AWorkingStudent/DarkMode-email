0fa8e0ec4a8aa0c25c23b446dcf279c03ff49d4e
eternalabu <abubakar.daud@mail.mcgill.ca>
2020-08-27 01:01:29 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import emailjs from "emailjs-com";
+import { ThemeProvider, createGlobalStyle } from 'styled-components'
+
+// dark mode code
+
+//dark mode colors
+const blue = "#0abab5"
+const gray = "#383718"
+
+
+//light mode colors
+const black = "#000000"
+const white = "#ffffff"
+
+const GlobalStyle = createGlobalStyle`
+body {
+  background-color: ${ props => props.theme.mode === 'dark' ? black : white };
+  color: ${props => props.theme.mode === 'dark' ? blue : gray };
+}`
+
+
+
+
+
+
+function Email(e) {
+    e.preventDefault();
+
+    emailjs.sendForm('abu', 'first', e.target, 'user_vvwrD3IeAVjQtDUQyWgjg')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+      e.target.reset()
+  }
+
+
+
+
+
+
+
+
+
+
 
 function App() {
+  const [theme, setTheme] = useState({ mode : 'light'});
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+    <ThemeProvider theme={theme}>
+
+      <>
+      <GlobalStyle/>
+      <header>
+        <h1>Hello world, email</h1>
+        <p>Sends hello world to any email in the forum</p>
       </header>
-    </div>
+
+
+        <div>
+        <form onSubmit={Email}>
+        <input type="email" placeholder="Your email address" name="email" />
+        <input type="submit" value="Send"/>
+        </form>
+        </div>
+
+        < button onClick={e=>setTheme(theme.mode === 'dark' ? {mode: 'light'} : {mode: 'dark'} )}> Dark Mode button</button>
+
+      <footer><p> made by Abubakar Daud</p></footer>
+      </>
+      </ThemeProvider>
+
+
   );
 }
 
---
+++ src/Theme.js
@@ -0,0 +1,25 @@
+
+
+const darkMode = {
+  background: gray,
+  body: blue
+
+};
+
+const lightMode = {
+
+  background: white,
+  body: black
+};
+
+const theme = mode => (mode === "dark" ? darkMode : lightMode);
+
+const
+
+
+
+
+
+
+
+export default theme
